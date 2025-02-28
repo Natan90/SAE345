19733058edf55855316edb61c249933fdfbfeb3d
Natan90 <natan.bouche@edu.univ-fcomte.fr>
2025-02-28 23:36:25 +0100

    push

--- controllers/admin_commande.py
+++ controllers/admin_commande.py
@@ -16,70 +16,60 @@ def admin_index():
     return render_template('admin/layout_admin.html')
 
 
-@admin_commande.route('/admin/commande/show', methods=['get','post'])
+@admin_commande.route('/admin/commande/show', methods=['get', 'post'])
 def admin_commande_show():
     mycursor = get_db().cursor()
 
-    admin_id = session['id_user']
-    sql = '''SELECT * FROM LIGNE_PANIER WHERE LIGNE_PANIER.id_utilisateur = %s'''
-    mycursor.execute(sql, admin_id)
-    items_ligne_panier = mycursor.fetchall()
-    if items_ligne_panier is None or len(items_ligne_panier) < 1:
-        flash('Votre panier est vide',)
-        return redirect('/client/panier/show')
-
-    date_commande= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    tuple_insert = (date_commande, admin_id, "1")
-    sql = '''INSERT INTO COMMANDE (date_achat, id_utilisateur, id_etat) VALUES (%s, %s, %s)'''
-    mycursor.execute(sql, tuple_insert)
-    sql = '''SELECT last_insert_id() as last_insert_id'''
+    sql = '''
+        SELECT C.id_commande, C.date_achat, U.login, 
+               COUNT(LC.id_equipement) AS nbr_equipements,
+               SUM(LC.prix * LC.quantite) AS prix_total,
+               E.libelle_etat AS etat
+        FROM COMMANDE C
+        JOIN UTILISATEUR U ON C.id_utilisateur = U.id_utilisateur
+        JOIN LIGNE_COMMANDE LC ON C.id_commande = LC.id_commande
+        JOIN ETAT E ON C.id_etat = E.id_etat
+        GROUP BY C.id_commande
+        ORDER BY C.date_achat DESC;
+    '''
     mycursor.execute(sql)
-    commande_id = mycursor.fetchone()
-    print(commande_id, tuple_insert)
-    print(items_ligne_panier)
-    print(admin_id)
-    print(commande_id['last_insert_id'])
-    print(commande_id)
+    commandes = mycursor.fetchall()
 
-    for item in items_ligne_panier:
-        sql = "DELETE FROM LIGNE_PANIER WHERE id_utilisateur = %s AND id_equipement =%s"
-        mycursor.execute(sql, admin_id, item['equipement_id'])
-        sql = "SELECT prix FROM EQUIPEMENT_SPORT WHERE id_equipement = %s"
-        mycursor.execute(sql, item['id_equipement'])
-        prix = mycursor.fetchone()
-        print(prix)
-        sql = "INSERT INTO LIGNE_COMMANDE (id_commande, id_equipement, prix, quantite) VALUES (%s, %s, %s, %s)"
-        tuple_insert = (commande_id['last_insert_id'], item['equipement_id'], prix['prix'], item['quantite'])
-        mycursor.execute(sql, tuple_insert)
-        get_db().commit()
-        id_client = session['id_user']
-        equipements_commande = None
-        commande_adresses = None
-        id_commande = request.args.get('id_commande', None)
-        print(id_commande)
-        if id_commande != None:
-            sql = ''' '''
-            commande_adresses = []
-        return render_template('admin/commandes/show.html'
-                               , equipements_commande=equipements_commande
-                               , commande_adresses=commande_adresses
-                               , id_commande=id_commande
-                               , id_client=id_client
+    id_commande = request.args.get('id_commande')
+    equipements_commande = []
 
-                               )
+    if id_commande:
+        sql = '''
+            SELECT EQ.nom_equipement AS nom, LC.quantite, LC.prix, 
+                   (LC.prix * LC.quantite) AS prix_ligne,
+                   C.id_etat AS etat_id, LC.id_commande AS id
+            FROM LIGNE_COMMANDE LC
+            JOIN EQUIPEMENT_SPORT EQ ON LC.id_equipement = EQ.id_equipement
+            JOIN COMMANDE C ON LC.id_commande = C.id_commande
+            WHERE LC.id_commande = %s
+        '''
+        mycursor.execute(sql, (id_commande,))
+        equipements_commande = mycursor.fetchall()
+
+    return render_template('admin/commandes/show.html',
+                           commandes=commandes,
+                           equipements_commande=equipements_commande)
 
 
 
-
-
-
-@admin_commande.route('/admin/commande/valider', methods=['get','post'])
+@admin_commande.route('/admin/commande/valider', methods=['POST'])
 def admin_commande_valider():
     mycursor = get_db().cursor()
-    commande_id = request.form.get('id_commande', None)
-    if commande_id != None:
-        print(commande_id)
-        sql = '''           '''
-        mycursor.execute(sql, commande_id)
+
+    commande_id = request.form.get('id_commande')
+
+    if commande_id:
+        print("Commande à valider:", commande_id)
+        sql = "UPDATE COMMANDE SET id_etat = 3 WHERE id_commande = %s"
+        mycursor.execute(sql, (commande_id,))
+
         get_db().commit()
-    return redirect('/admin/commande/show')
+
+        return redirect('/admin/commande/show')
+
+    return "Erreur : commande non trouvée", 400
