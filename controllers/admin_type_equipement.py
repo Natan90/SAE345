4842830f2d1e488f2d88cf655dbea7a62516a42f
levard emmanuel <emmanuel.levard@edu.univ-fcomte.fr>
2025-02-28 21:52:56 +0100

    il manque juste le add

--- controllers/admin_type_equipement.py
+++ controllers/admin_type_equipement.py
@@ -27,11 +27,12 @@ def add_type_equipement():
 @admin_type_equipement.route('/admin/type-equipement/add', methods=['POST'])
 def valid_add_type_equipement():
     libelle = request.form.get('libelle', '')
-    tuple_insert = (libelle,)
-    mycursor = get_db().cursor()
-    sql = '''         '''
+    tuple_insert = (libelle)
+    conn=get_db()
+    mycursor = conn.cursor()
+    sql = '''    INSERT INTO TYPE_EQUIPEMENT_SPORT VALUES (NULL,%s)   '''
     mycursor.execute(sql, tuple_insert)
-    get_db().commit()
+    conn.commit()
     message = u'type ajouté , libellé :'+libelle
     flash(message, 'alert-success')
     return redirect('/admin/type-equipement/show') #url_for('show_type_equipement')
@@ -40,6 +41,15 @@ def valid_add_type_equipement():
 def delete_type_equipement():
     id_type_equipement = request.args.get('id_type_equipement', '')
     mycursor = get_db().cursor()
+    sql="""   SELECT COUNT(id_equipement) as nbr_dep from EQUIPEMENT_SPORT where id_type_equipement_sport=%s"""
+    mycursor.execute(sql, id_type_equipement)
+    result=mycursor.fetchone()
+    if result['nbr_dep']!=0:
+        flash("Nombre de dépendences non null, il y a {} dépendences".format(result['nbr_dep']))
+        return redirect("/admin/type-equipement/show")
+    sql="""DELETE FROM TYPE_EQUIPEMENT_SPORT WHERE id_type_equipement_sport = %s"""
+    mycursor.execute(sql, id_type_equipement)
+    get_db().commit()
 
     flash(u'suppression type equipement , id : ' + id_type_equipement, 'alert-success')
     return redirect('/admin/type-equipement/show')
@@ -48,8 +58,8 @@ def delete_type_equipement():
 def edit_type_equipement():
     id_type_equipement = request.args.get('id_type_equipement', '')
     mycursor = get_db().cursor()
-    sql = '''   '''
-    mycursor.execute(sql, (id_type_equipement,))
+    sql = ''' SELECT * FROM TYPE_EQUIPEMENT_SPORT WHERE id_type_equipement_sport=%s  '''
+    mycursor.execute(sql, (id_type_equipement))
     type_equipement = mycursor.fetchone()
     return render_template('admin/type_equipement/edit_type_equipement.html', type_equipement=type_equipement)
 
@@ -59,7 +69,7 @@ def valid_edit_type_equipement():
     id_type_equipement = request.form.get('id_type_equipement', '')
     tuple_update = (libelle, id_type_equipement)
     mycursor = get_db().cursor()
-    sql = '''   '''
+    sql = ''' UPDATE TYPE_EQUIPEMENT_SPORT SET libelle_type_equipement_sport = %s WHERE  id_type_equipement_sport = %s  '''
     mycursor.execute(sql, tuple_update)
     get_db().commit()
     flash(u'type equipement modifié, id: ' + id_type_equipement + " libelle : " + libelle, 'alert-success')
